60cfa4a889366d34d81900f6beeec784af48b1bc
nadakoga <nadokhojah@hotmail.com>
2018-12-09 22:13:03 +0200

    Add files via upload

---
+++ Project/Museum Client/src/app/community/community.module.ts
@@ -0,0 +1,20 @@
+// Angular Imports
+import { NgModule } from '@angular/core';
+
+// This Module's Components
+import { CommunityComponent } from './community.component';
+
+@NgModule({
+    imports: [
+
+    ],
+    declarations: [
+        CommunityComponent,
+    ],
+    exports: [
+        CommunityComponent,
+    ]
+})
+export class CommunityModule {
+
+}
